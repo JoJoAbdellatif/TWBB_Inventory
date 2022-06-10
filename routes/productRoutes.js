df220a6673d0fa6923664c86437fbd9bf68684b9
XXXJoJoXXX <ymostafa2010@gmail.com>
2022-06-10 20:19:00 +0200

    updated Inventory service

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,11 +5,11 @@ const asyncHandler = require('express-async-handler');
 const product = require('../models/product');
 const productPrice = require('../models/price_quant');
 
-
 const productRoute = express.Router();
 
 productRoute.get('/details/:id',asyncHandler( async (req , res) => {
     const productExist = await product.findOne({ _id:req.params.id});
+    
     if(!productExist){
         throw new Error('Product does not exist');
     }
@@ -26,6 +26,18 @@ productRoute.get('/price/:id',asyncHandler( async (req , res) => {
     res.status(200).json(prodPrice);
 
 }))
+productRoute.get('/details&price/:id',asyncHandler(async(req,res)=>{
+    const productExist = await product.findOne({ _id:req.params.id});
+    if(!productExist){
+        throw new Error('Product does not exist');
+    }
+    const prodPrice = await productPrice.findOne({ productID:req.params.id});
+
+    const output = {'Name':productExist.productName,'Category':productExist.productCategory,'Price':prodPrice.productPrice,'Description':productExist.productDescription,'Weight':productExist.productWeight,'Image':productExist.productImage,"QuantityAvaliable":prodPrice.productQuantity}
+
+    res.status(200).send(output)
+}))
+
 
 
 productRoute.get('/',asyncHandler( async (req , res) => {
@@ -57,17 +69,27 @@ productRoute.get('/search',asyncHandler( async (req , res) => {
     const searchInput = req.query.searchInput||""
     const page = req.query.page||0
     const regex = new RegExp(searchInput, 'i') 
+    let l=''
     const products = await product.find({
         productName: regex,
       }).skip(page*productPerPage).limit(productPerPage).then((productsInPage)=>{
         
-        res.status(200).json(productsInPage)
+        l = productsInPage
     }).catch(()=>{
         new Error('No Products Found')
         res.status(500)
     })
+    var purchacelist=[];
+    for (let i = 0; i < l.length; i++) {
+        const price = await productPrice.findOne({productID:l[i]._id})
+        const obj = {'ProductID':l[i]._id,'ProductName':l[i].productName,'ProductDescription':l[i].productDescription,'ProductCategory':l[i].productCategory,'ProductWeight':l[i].productWeight,'ProductImage':l[i].productImage,'Productprice':price.productPrice,'ProductQuantity':price.productQuantity}
+     
+        purchacelist.push(obj)
+      }
+    res.send(purchacelist)
     
 }))
+
 
 productRoute.post('/addProduct' , asyncHandler(async (req , res) => {
     let filePath = 'inventoryService/downloads/Products.csv';
@@ -81,7 +103,7 @@ productRoute.post('/addProduct' , asyncHandler(async (req , res) => {
                 
             }
             else{
-                const prod =await product.create({productName : productJSON[i].productName , productDescription : productJSON[i].productDescription , productCategory : productJSON[i].productCategory , productWeight : productJSON[i].productWeight});
+                const prod =await product.create({productName : productJSON[i].productName , productDescription : productJSON[i].productDescription , productCategory : productJSON[i].productCategory , productWeight : productJSON[i].productWeight,productImage:productJSON[i].productImages});
                 await productPrice.create({productID:prod._id,productPrice:productJSON[i].productPrice,productQuantity:productJSON[i].productQuantity})
                 counter+=1;
             }
@@ -115,6 +137,19 @@ productRoute.patch('/updateProducts' , asyncHandler(async (req , res) => {
 
 }))
 
+productRoute.patch('/quantAfterOrder',asyncHandler( async (req , res) => {
+    const prodPrice = await productPrice.findOne({ productID:req.query.productId });
+    if(!prodPrice){
+        throw new Error('Product does not exist');
+    }
+    else if(prodPrice.productQuantity == 0){
+        throw new Error('Out of stock');
+    }
+    
+    await productPrice.findOne({productID:prodPrice.productID}).updateOne({productQuantity: (parseInt(prodPrice.productQuantity)- parseInt(req.query.productQuant)).toString()});
+    res.send('Updated Quantity')
+}))
+       
 
 module.exports = productRoute
 
